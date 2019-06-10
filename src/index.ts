c31d1f6a63917301aa4649d643b67d198b10e790
biqing.hu <biqing.hu@zhaopin.com.cn>
2019-06-10 16:16:00 +0800

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-const Koa = require('koa')
-const { ApolloServer, gql } = require('apollo-server-koa')
+import * as Koa from 'koa'
+import * as Router from 'koa-router'
+import { ApolloServer, gql } from 'apollo-server-koa'
+import koaPlayground from 'graphql-playground-middleware-koa'
+
+const {
+  execute,
+  parse,
+  validateSchema,
+  validate,
+  GraphQLError
+} = require('graphql')
+
+const router = new Router()
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -10,9 +22,9 @@ const typeDefs = gql`
 
 // Provide resolver functions for your schema fields
 const resolvers = {
-    Query: {
-        hello: () => 'Hello world!',
-    },
+  Query: {
+    hello: () => 'Hello world!',
+  },
 }
 
 const server = new ApolloServer({ typeDefs, resolvers })
@@ -20,6 +32,17 @@ const server = new ApolloServer({ typeDefs, resolvers })
 const app = new Koa()
 server.applyMiddleware({ app })
 
+
+router.all('/playground', koaPlayground({ endpoint: '/graphql' }))
+
+router.get('/test', ctx => {
+  ctx.response.body = { a: 1 }
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
 app.listen({ port: 4000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
 )
